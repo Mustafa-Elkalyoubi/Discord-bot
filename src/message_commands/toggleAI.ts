2cb4ed1f4a81b0f358321c650a8db5e583f31551
Mustafa <mustafa.elkalyoubi@gmail.com>
2023-08-25 13:59:25 +0400

    added alias and status

--- src/message_commands/toggleAI.ts
+++ src/message_commands/toggleAI.ts
@@ -1,13 +1,17 @@
-import { Message } from "discord.js";
+import { ActivityType, Message } from "discord.js";
 import ExtendedClient from "../utils/Client";
 
 exports.run = async function run(message: Message, args: string[], client: ExtendedClient) {
   client.aiEnabled = !client.aiEnabled;
+  client.user?.setActivity(
+    client.aiEnabled ? "Text2Img is enabled :)" : "Text2Img is disabled :(",
+    { type: ActivityType.Playing }
+  );
   message.reply(`text2img is now ${client.aiEnabled ? "enabled" : "disabled"}`);
 };
 
 exports.conf = {
-  aliases: ["toggleAI", "toggleai", "toggleAi", "toggle"],
+  aliases: ["toggleAI", "toggleai", "toggleAi", "toggle", "ai"],
   permLevel: 4,
 };
 
