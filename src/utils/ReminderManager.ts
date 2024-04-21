cdbbfe6e2ff2e6a1d976ec2cbbf58c880bc047a1
Mustafa <mustafa.elkalyoubi@gmail.com>
2024-04-21 17:15:10 +0400

    json to database migration

--- src/utils/ReminderManager.ts
+++ src/utils/ReminderManager.ts
@@ -1,86 +1,110 @@
-import { ChatInputCommandInteraction, Collection } from "discord.js";
-import fs from "node:fs";
-import path from "path";
+import { ChatInputCommandInteraction } from "discord.js";
 import { DateTime } from "luxon";
+import { RecurrenceRule, scheduleJob, scheduledJobs } from "node-schedule";
 
-import type { ReminderDetails, ReminderSaveType } from "../types.js";
-import ExtendedClient from "./Client.js";
+import mongoose, { Types } from "mongoose";
+import UserData from "../models/UserData";
+import type { ReminderDetails } from "../types";
+import ExtendedClient from "./Client";
 
 export default class ReminderManager {
-  #reminders: Collection<string, Array<ReminderDetails>>;
-  #reminderTimeouts: NodeJS.Timeout[];
-
-  #reminderPath: string;
   #client: ExtendedClient;
 
-  constructor(client: ExtendedClient, basePath: string) {
-    this.#reminderPath = path.join(basePath, "reminders.json");
+  constructor(client: ExtendedClient) {
     this.#client = client;
-
-    this.#reminders = new Collection(JSON.parse(fs.readFileSync(this.#reminderPath, "utf-8")));
-    this.#reminderTimeouts = [];
-    this.reloadTimeouts();
+    this.#loadJobs();
   }
 
-  get(reminderID: string) {
-    return this.#reminders.get(reminderID);
+  async #loadJobs() {
+    const reminders = await UserData.aggregate([
+      {
+        $unwind: "$reminders",
+      },
+      {
+        $project: {
+          _id: "$reminders._id",
+          userID: true,
+          message: "$reminders.message",
+          timeToRemind: "$reminders.timeToRemind",
+          channel: "$reminders.channel",
+          recurring: "$reminders.recurring",
+          details: "$reminders.details",
+        },
+      },
+    ]);
+
+    if (!reminders) throw "Failed to load reminders";
+
+    reminders.forEach((reminder) => {
+      if (DateTime.now() >= reminder.timeToRemind) return this.execute(reminder, reminder.userID);
+
+      if (!reminder.recurring)
+        scheduleJob(reminder._id.toString(), reminder.timeToRemind, () => {
+          this.execute(reminder, reminder.userID);
+        });
+      else {
+        const rule = new RecurrenceRule(
+          undefined,
+          undefined,
+          undefined,
+          reminder.details.day,
+          reminder.details.hour,
+          reminder.details.minute
+        );
+        scheduleJob(reminder._id.toString(), rule, () => {
+          this.execute(reminder, reminder.userID);
+        });
+      }
+    });
+  }
+
+  async getUserReminders(userID: string) {
+    return (await UserData.findOne({ userID }))?.reminders ?? [];
+  }
+
+  async get(reminderID: string) {
+    return UserData.findOne({
+      reminders: {
+        $elemMatch: {
+          id: reminderID,
+        },
+      },
+    }).get(reminderID);
   }
 
   execute(reminder: ReminderDetails, userID: string) {
     this.#client.sendToChannel(reminder.channel, `<@!${userID}>: ${reminder.message}`);
 
-    if (!reminder.recurring) this.remove(reminder.id, userID);
-    else {
-      if (!reminder.details) throw "Recurring details missing in reminder";
-      const { day, hour, minute } = reminder.details;
-      let newReminderTime = DateTime.fromFormat(`${day} ${hour} ${minute}`, "ccc H m");
-      if (newReminderTime.toMillis() < DateTime.now().toMillis())
-        newReminderTime = newReminderTime.plus({ days: 7 });
-      reminder.timeToRemind = newReminderTime.toMillis();
-    }
-
-    this.reloadTimeouts();
-    this.#export();
+    if (!reminder.recurring) this.remove(reminder._id, userID);
   }
 
-  remove(reminderID: number, userID: string) {
-    const reminders = this.#reminders.get(userID);
-    if (!reminders) throw "user has no reminders to delete";
+  async remove(reminderID: Types.ObjectId, userID: string) {
+    await UserData.updateOne(
+      {
+        userID,
+      },
+      {
+        $pull: {
+          reminders: {
+            _id: reminderID,
+          },
+        },
+      },
+      { upsert: false }
+    );
 
-    const reminderToDelete = reminders.findIndex((r) => r.id === reminderID);
-    if (reminderToDelete === -1) throw "cannot find reminder to delete";
-
-    reminders.splice(reminderToDelete, 1);
-    if (reminders.length < 1) this.#reminders.delete(userID);
-
-    this.#export();
-  }
-
-  reloadTimeouts() {
-    for (const timeout of this.#reminderTimeouts) clearTimeout(timeout);
-
-    this.#reminderTimeouts = [];
-    this.#reminders.forEach((reminders, userID) => {
-      reminders.forEach((reminder) => {
-        if (reminder.timeToRemind - DateTime.now().toMillis() < 2 ** 32 - 1) {
-          const timeout = setTimeout(() => {
-            this.execute(reminder, userID);
-          }, reminder.timeToRemind - DateTime.now().toMillis());
-          this.#reminderTimeouts.push(timeout);
-        }
-      });
-    });
+    if (reminderID.toString() in scheduledJobs) scheduledJobs[reminderID.toString()].cancel();
   }
 
   async save(
     interaction: ChatInputCommandInteraction,
     message: string,
-    timeToRemind: number
+    timeToRemind: DateTime
   ): Promise<void>;
   async save(
     interaction: ChatInputCommandInteraction,
     message: string,
-    timeToRemind: number,
+    timeToRemind: DateTime,
     recurring: true,
     day: string,
     hour: number,
@@ -89,21 +113,24 @@ export default class ReminderManager {
   async save(
     interaction: ChatInputCommandInteraction,
     message: string,
-    timeToRemind: number,
+    timeToRemind: DateTime,
     recurring = false,
     day?: string,
     hour?: number,
     minute?: number
   ): Promise<void> {
     const userID = interaction.user.id;
-    const reminders = this.#reminders.ensure(userID, () => []);
 
     if (!interaction.channel) throw "No idea how this error occurred";
+
+    let user = await UserData.findOne({ userID: userID });
+    if (!user) user = new UserData({ userID, username: interaction.user.username });
+
     const saveDetails = {
-      id: DateTime.now().toMillis() + parseInt(userID),
+      _id: new mongoose.Types.ObjectId(),
       channel: interaction.channel.id,
-      message: message,
-      timeToRemind: timeToRemind,
+      message,
+      timeToRemind: timeToRemind.toJSDate()!,
       recurring: recurring,
       ...(recurring && {
         details: {
@@ -114,17 +141,15 @@ export default class ReminderManager {
       }),
     } as ReminderDetails;
 
-    reminders.push(saveDetails);
-    this.#reminders.set(userID, reminders);
+    user.reminders.push(saveDetails);
 
     try {
-      const successful = this.#export();
-      if (!successful) throw "Check Client.ts: 268";
+      user.save();
 
       if (saveDetails.recurring) {
         interaction.reply({
-          content: `\`RECURRING\` Reminder [${saveDetails.id}] set! Reminding you <t:${Math.floor(
-            timeToRemind / 1000
+          content: `\`RECURRING\` Reminder [${saveDetails._id}] set! Reminding you <t:${Math.floor(
+            timeToRemind.toSeconds()
           )}:R> (Every ${DateTime.fromFormat(`${day} ${hour} ${minute}`, "ccc H m").toFormat(
             "EEEE 'at' H':'mm"
           )})`,
@@ -132,30 +157,16 @@ export default class ReminderManager {
         return;
       }
       interaction.reply({
-        content: `Reminder \`${saveDetails.id}\` set! Reminding you <t:${Math.floor(
-          timeToRemind / 1000
+        content: `Reminder \`${saveDetails._id}\` set! Reminding you <t:${Math.floor(
+          timeToRemind.toSeconds()
         )}:R>`,
       });
 
-      if (saveDetails.timeToRemind - DateTime.now().toMillis() < 2 ** 32 - 1)
-        setTimeout(() => {
-          this.execute(saveDetails, userID);
-        }, saveDetails.timeToRemind - DateTime.now().toMillis());
+      scheduleJob(saveDetails._id.toString(), timeToRemind.toJSDate()!, () => {
+        this.execute(saveDetails, userID);
+      });
     } catch (err) {
       interaction.reply({ content: "Sorry, something went wrong", ephemeral: true });
-    }
-  }
-
-  #export() {
-    try {
-      fs.writeFileSync(
-        this.#reminderPath,
-        JSON.stringify(Array.from(this.#reminders.entries()) satisfies ReminderSaveType, null, 4)
-      );
-      return true;
-    } catch (e) {
-      console.error(e);
-      return false;
     }
   }
 }
