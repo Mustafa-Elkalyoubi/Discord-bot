12a4367c9ac67c8bde93dc6c71d7fd25f08847b7
Mustafa <mustafa.elkalyoubi@gmail.com>
2024-04-21 17:14:40 +0400

    ignored valid uses of any

--- src/utils/index.d.ts
+++ src/utils/index.d.ts
@@ -5,9 +5,12 @@ declare class Stringified<T> extends String {
 interface JSON {
   stringify<T>(
     value: T,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     replacer?: (key: string, value: any) => any,
     space?: string | number
   ): string & Stringified<T>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   parse<T>(text: Stringified<T>, reviver?: (key: any, value: any) => any): T;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   parse(text: string, reviver?: (key: any, value: any) => any): any;
 }
